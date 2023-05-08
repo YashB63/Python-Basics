5b37e95f81390be62f04d5fb594bec920a849e6c
Yash Bhagwani <83640238+YashB63@users.noreply.github.com>
2023-05-09 03:00:59 +0530

    Add files via upload

---
+++ Python basics/indexing_and_slicing.py
@@ -0,0 +1,18 @@
+yash_string = 'GoodMorning'
+#--------Indexing--------
+print(yash_string[0])#0th character print hoga
+print(yash_string[7])#8th character print hoga
+
+print(yash_string[-1])#last character print hoga
+print(yash_string[-4])#piche se 4th element print hoga
+
+#----------Slicing------------
+print(yash_string[:3])#starting ke 3 characters print hoge
+print(yash_string[4:])#starting ke 4 chor kr saare print hoge
+
+print(yash_string[3:6])#index 3 se shuru ho kr index 6 waale characters print hoge
+print(yash_string[2:7:2])#yaha kya hoga ki pehla 2-7 tk characters chahiye usme se saare nahi chahiye har dusri step pr jo character hai vo chahiye.
+#But dhyan rkhna hai ki print ki staring index 2 waale element se hi hogi. and step size will be 2.
+
+#reversing a string ka ek method
+print(yash_string[::-1])#one line method
