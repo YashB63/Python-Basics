41f369c35e675ba3cffb617b5d1bf35ad43c5789
Yash Bhagwani <83640238+YashB63@users.noreply.github.com>
2023-05-15 22:49:25 +0530

    Add files via upload

---
+++ Tuple and Set/tuple_n_set.py
@@ -0,0 +1,13 @@
+#round bracket defines its a tuple
+tup = (1,2,37,4,55)
+print(tup)
+#tuples ki values change nahi hoti hai.
+
+
+#set is a collection of unique elements
+#set  is denotes by curly brackets
+s = {23,54,14,67,50}
+print(s) 
+
+s = {25,14,98,63,75,98}
+print(s)
